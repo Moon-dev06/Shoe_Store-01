aa07eb2c69989568480456bcc6503b6466633c34
Kun_BNdev06 <kun26kun49@gmail.com>
2026-03-01 16:31:12 +0700

    feat: add The Craft section and update frontend image to v2

--- load-test.js
+++ load-test.js
@@ -2,12 +2,13 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-    vus: 100, // จำลองคนเข้าพร้อมกัน 100 คน
+    vus: 1, // จำลองคนเข้าพร้อมกัน 100 คน
     duration: '1m',
 };
 
 export default function () {
     // เปลี่ยนจาก <YOUR_APP_URL> เป็น localhost:31023
-    http.get('http://localhost:31023/');
+    // http.get('http://localhost:31023');
+    http.get('http://localhost:31023/api/health');
     sleep(1);
 }
